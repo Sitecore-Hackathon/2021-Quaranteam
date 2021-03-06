db451ab9f19c5c3c309181594913516ab1bb5e5f
Szymon Dabrowski <szymon.dabrowski@americaneagle.com>
2021-03-06 15:07:03 -0600

    Adds new module model

---
+++ src/Foundation/Reflector/website/Models/NewModuleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+
+namespace Hackathon.Foundation.Reflector.Models
+{
+    public class NewModuleModel
+    {
+        public string ContentTypeName { get; set; }
+
+        public Item PageTemplate { get; set; }
+
+        public List<Item> BaseTemplates { get; set; }
+        public Dictionary<string,Item> FieldList { get; set; }
+    }
+}
