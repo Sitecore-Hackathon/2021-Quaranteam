33dda93ee89618d1318a4d61beab64e638a6c111
James Gregory <digitalparkour@gmail.com>
2021-03-06 01:07:55 -0600

    Yay, can compile and deploy

---
+++ src/Foundation/DependencyInjection/website/IoC/MvcControllerServicesConfigurator.cs
@@ -0,0 +1,18 @@
+﻿namespace Hackathon.Foundation.DependencyInjection.IoC
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Sitecore.DependencyInjection;
+
+    public class MvcControllerServicesConfigurator : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddMvcControllers("Hackathon.Feature.*");
+            serviceCollection.AddApiControllers("Hackathon.Feature.*");
+            serviceCollection.AddClassesWithServiceAttribute("Hackathon.Feature.*");
+            serviceCollection.AddMvcControllers("Hackathon.Foundation.*");
+            serviceCollection.AddApiControllers("Hackathon.Foundation.*");
+            serviceCollection.AddClassesWithServiceAttribute("Hackathon.Foundation.*");
+        }
+    }
+}
---
+++ src/Foundation/Reflector/website/README.md
@@ -0,0 +1,3 @@
+# Reflector
+
+Write a few words about the module here..
